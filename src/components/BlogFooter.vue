bba0ed337027d922320bf2aad76d36f80e0b3781
Jason Springer <jsnsprngr@gmail.com>
2019-03-06 22:07:48 -0500

    first commit

---
+++ src/components/BlogFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <transition appear name="v--mask">
+    <footer v-once class="blog__footer">
+      <a class="signature" href="" target="_blank">
+      </a>
+    </footer>
+  </transition>
+</template>
+
+<script>
+export default { name: 'blog-footer' }
+</script>
